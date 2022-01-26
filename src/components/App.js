676a016aee13c10e6f965002e9de9e6703e9b313
용석 박 <4up456@naver.com>
2022-01-26 15:11:03 +0900

    처음 랜더링시에만 실행

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import ReviewList from './RevieList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getReviews } from '../api';
 
 function App() {
@@ -16,10 +16,14 @@ function App() {
     setItems(nextItems);
   };
 
-  const handleLoadClick = async () => {
+  const handleLoad = async () => {
     const { reviews } = await getReviews();
     setItems(reviews);
   };
+
+  useEffect(() => {
+    handleLoad();
+  }, []);
 
   return (
     <div>
@@ -28,7 +32,6 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
