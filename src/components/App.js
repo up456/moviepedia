faf92d0177b82ef48dfdfac64ef7911e6bfffc3e
용석 박 <4up456@naver.com>
2022-01-26 15:03:38 +0900

    fetch사용하기

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import ReviewList from './RevieList';
-import mockItems from '../mock.json';
 import { useState } from 'react';
+import { getReviews } from '../api';
 
 function App() {
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
   const [order, setOrder] = useState('createdAt');
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -16,6 +16,11 @@ function App() {
     setItems(nextItems);
   };
 
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews();
+    setItems(reviews);
+  };
+
   return (
     <div>
       <div>
@@ -23,6 +28,7 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
