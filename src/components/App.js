6a843b5b00b0a0815d6712f067fc763a21b1f32c
용석 박 <4up456@naver.com>
2022-01-26 22:16:42 +0900

    에러처리

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ function App() {
   const [offset, setOffset] = useState(0);
   const [hasNext, setHasNext] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(null);
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -27,9 +28,10 @@ function App() {
 
     try {
       setIsLoading(true);
+      setLoadingError(null);
       result = await getReviews(options);
     } catch (error) {
-      console.error(error);
+      setLoadingError(error);
       return;
     } finally {
       setIsLoading(false);
@@ -64,6 +66,7 @@ function App() {
           더 보기
         </button>
       )}
+      {loadingError?.message && <span>{loadingError.message}</span>}
     </div>
   );
 }
