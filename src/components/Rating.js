3de822abd4f07b29d278f1bc2921f6fa90616cd9
용석 박 <4up456@naver.com>
2022-01-28 13:45:50 +0900

    별점 입력

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -2,16 +2,35 @@ import './Rating.css';
 
 const RATINGS = [1, 2, 3, 4, 5];
 
-function Star({ selected = false }) {
+function Star({ selected = false, rating, onSelect, onHover }) {
   const className = `Rating-star ${selected ? 'selected' : ''}`;
-  return <span className={className}>★</span>;
+
+  const handleClick = onSelect ? () => onSelect(rating) : undefined;
+
+  const handleMouseOver = onHover ? () => onHover(rating) : undefined;
+
+  return (
+    <span
+      className={className}
+      onClick={handleClick}
+      onMouseOver={handleMouseOver}
+    >
+      ★
+    </span>
+  );
 }
 
-function Rating({ value = 0 }) {
+function Rating({ className, value = 0, onSelect, onHover, onMouseOut }) {
   return (
-    <div>
+    <div className={className} onMouseOut={onMouseOut}>
       {RATINGS.map((rating) => (
-        <Star key={rating} selected={value >= rating} />
+        <Star
+          key={rating}
+          selected={value >= rating}
+          rating={rating}
+          onSelect={onSelect}
+          onHover={onHover}
+        />
       ))}
     </div>
   );
